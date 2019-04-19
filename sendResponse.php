25a3baab17535d5f4b3f7c0e89fbc71524dd8d14
Logan <laforsythe@email.arizona.edu>
2019-04-18 21:51:04 -0700

    uncommented php code

--- sendResponse.php
+++ sendResponse.php
@@ -25,14 +25,14 @@ if(isset($_GET['website'])) {
         $read = stream_socket_client("ssl://" . $orignal_parse . ":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
         $cert = stream_context_get_params($read);
         $certinfo = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
-        print_r($certinfo);
+      //  print_r($certinfo);
 
 
-      /*  if ($certinfo){
+        if ($certinfo){
             echo $site . ' is secure!';
         } else{
             echo $site . ' is not secure!';
-        }  */
+        }  
     }
 }
 
